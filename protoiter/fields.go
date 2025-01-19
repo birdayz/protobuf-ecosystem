f56895895478c52fa702a0f33e9fc4d9a082477a
Johannes Brüderl <johannes.bruederl@gmail.com>
2025-01-19 02:05:59 +0100

    protoiter: include unset fields if map value is message

--- protoiter/fields.go
+++ protoiter/fields.go
@@ -10,25 +10,25 @@ import (
 
 // Fields return an iterator that iterates over all fields.
 func Fields(m protoreflect.Message) iter.Seq[protopath.Values] {
-	return func(f func(protopath.Values) bool) {
+	return func(yield func(protopath.Values) bool) {
 		_ = protorange.Range(m, func(v protopath.Values) error {
-			if !f(v) {
+			if !yield(v) {
 				return protorange.Terminate
 			}
 
 			// In addition to the "normal" calls to the provided function f, also call it for unset primitive fields.
 			leaf := v.Index(-1)
 
-			// Inject for messages in lists.
+			// Inject for items in lists.
 			if leaf.Step.Kind() == protopath.ListIndexStep {
-
 				parent := v.Index(-2)
+				// If item in the list is a message, inject its fields!
 				if parent.Step.FieldDescriptor().Kind() == protoreflect.MessageKind {
 					msg := parent.Value.List().Get(leaf.Step.ListIndex()).Message()
 					for i := 0; i < msg.Descriptor().Fields().Len(); i++ {
 						fd := msg.Descriptor().Fields().Get(i)
 						if !msg.Has(fd) {
-							if !f(protopath.Values{
+							if !yield(protopath.Values{
 								Path:   append(v.Path, protopath.FieldAccess(fd)),
 								Values: append(v.Values, fd.Default()),
 							}) {
@@ -38,16 +38,40 @@ func Fields(m protoreflect.Message) iter.Seq[protopath.Values] {
 
 					}
 				}
+				return nil
 			}
 
-			// Inject for messages in message-fields.
-			if (leaf.Step.Kind() == protopath.RootStep) || (leaf.Step.Kind() == protopath.FieldAccessStep && leaf.Step.FieldDescriptor().Kind() == protoreflect.MessageKind && !leaf.Step.FieldDescriptor().IsMap() && !leaf.Step.FieldDescriptor().IsList()) {
+			if leaf.Step.Kind() == protopath.MapIndexStep {
+				parent := v.Index(-2)
+				// If item in the list is a message, inject its fields!
+				if parent.Step.FieldDescriptor().MapValue().Kind() == protoreflect.MessageKind {
+					msg := parent.Value.Map().Get(leaf.Step.MapIndex()).Message()
+					for i := 0; i < msg.Descriptor().Fields().Len(); i++ {
+						fd := msg.Descriptor().Fields().Get(i)
+						if !msg.Has(fd) {
+							if !yield(protopath.Values{
+								Path:   append(v.Path, protopath.FieldAccess(fd)),
+								Values: append(v.Values, fd.Default()),
+							}) {
+								return protorange.Terminate
+							}
+						}
+
+					}
+				}
+				return nil
+			}
+
+			// Inject for fields in messages.
+			if (leaf.Step.Kind() == protopath.RootStep) ||
+				(leaf.Step.Kind() == protopath.FieldAccessStep && leaf.Step.FieldDescriptor().Kind() == protoreflect.MessageKind &&
+					!leaf.Step.FieldDescriptor().IsMap() && !leaf.Step.FieldDescriptor().IsList()) {
 				msg := leaf.Value.Message()
 
 				for i := 0; i < msg.Descriptor().Fields().Len(); i++ {
 					fd := msg.Descriptor().Fields().Get(i)
 					if !msg.Has(fd) {
-						if !f(protopath.Values{
+						if !yield(protopath.Values{
 							Path:   append(v.Path, protopath.FieldAccess(fd)),
 							Values: append(v.Values, fd.Default()),
 						}) {
